c6dea4e05b182721c269d8373c70bccc2377def7
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2022-05-11 16:59:50 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/* Created by: marco cuconato
+   Created on: April 2022
+   This file contains the JS functions for index.html*/
+
+'use strict'
+
+/**
+ * Check service worker.
+ */
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-07-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-07-HTML/",
+  })
+}
+
+/**
+ * Input.
+ */
+
+function buttonClicked() {
+  
+  const integer = document.getElementById("integer").value
+  var integerValue = 0
+  var answer = 0
+
+  if (integer >= 0) {
+    for (let count = 0; count < integer; count++) {
+      integerValue++
+      answer = answer + integerValue
+    }
+  } else 
+    for (let count = 0; count > integer; count--) {
+      integerValue--
+      answer = answer + integerValue
+    }
+    document.getElementById("output").innerHTML = answer
+}
